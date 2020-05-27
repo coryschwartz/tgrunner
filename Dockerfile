0b20cc144aed530b51cb23a8313d1aceef404049
Cory Schwartz <cory@protocol.ai>
2020-05-26 23:18:19 -0700

    scaling

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
 FROM iptestground/testground:edge
 
+# Install kops and kubectl
+RUN wget -O /usr/local/bin/kops  https://github.com/kubernetes/kops/releases/download/v1.16.2/kops-linux-amd64 && chmod +x /usr/local/bin/kops
+RUN wget -O /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl && chmod +x /usr/local/bin/kubectl
+
+# Kops scaling files
+COPY small.yaml small.yaml
+COPY large.yaml large.yaml
+
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,14 @@ echo instances: "$INSTANCES"
 echo daemon: "$DAEMON"
 
 
+# Scale up
+/usr/local/bin/kops export kubecfg --name "$NAME"
+/usr/local/bin/kops replace -f large.yaml --force
+/usr/local/bin/kops kops update cluster --name "$NAME" --yes
+/usr/local/bin/kops validate cluster --wait 600s
+
+
+# run testground
 /usr/local/bin/testground plan import --git --from "$GITREPO"
 /usr/local/bin/testground --endpoint "$DAEMON" run single           \
 	                             --builder docker:go            \
@@ -15,3 +23,7 @@ echo daemon: "$DAEMON"
 				     --build-cfg registry_type=aws  \
 				     --build-cfg push_registry=true \
 				     --build-cfg go_proxy_mode=direct
+
+# Scale down
+/usr/local/bin/kops delete ig nodes --yes
+/usr/local/bin/kops update cluster --name "$NAME" --yes
