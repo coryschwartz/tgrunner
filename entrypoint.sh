69a3be7e9d17db798b5cf80b0c7c98875384583c
Will Scott <willscott@gmail.com>
2020-05-27 17:39:32 -0700

    add ingress

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,3 +27,7 @@ echo daemon: "$DAEMON"
 # Scale down
 /usr/local/bin/kops replace -f small.yaml --force
 /usr/local/bin/kops update cluster --yes --name "$NAME"
+
+# re-apply ingress
+/usr/local/bin/kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/aws/deploy.yaml
+/usr/local/bin/kubectl apply -f ingress.yaml
